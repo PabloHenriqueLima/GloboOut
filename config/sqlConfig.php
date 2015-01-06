56d0685c69b4289a6d621d366afc67e75cc1d6e5
PabloHenriqueLima <pabllo_sjp@hotmail.com>
2015-01-06 15:38:48 -0300

    Final - Mod

--- config/sqlConfig.php
+++ config/sqlConfig.php
@@ -1,14 +1,16 @@
 <?php
 /** Jesus Cristo - O Senhor e Salvador da Terra. **/
-$ip_mysql ="127.0.0.1";
-$login_mysql ="root";
-$senha_mysql ="";
-$database_mysql="globodb";
+
+$ip_mysql ="dbmy0053.whservidor.com";
+$login_mysql ="globoinfor1";
+$senha_mysql ="GloboSec2415";
+$database_mysql="globoinfor1";
 $error_msg ="Não foi possível conectar ao banco de dados: ";
+
+
 
 $mysqli = new mysqli($ip_mysql,$login_mysql,$senha_mysql,$database_mysql);
 $mysqli->set_charset("utf8");
 if (mysqli_connect_errno()) {
     die($error_msg . mysqli_connect_error());
-    exit();
 }
